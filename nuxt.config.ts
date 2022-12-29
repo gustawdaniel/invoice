43f7696b66e6c0462459789bd9e8726d8608824b
Daniel Gustaw <gustaw.daniel@gmail.com>
2022-12-29 15:35:07 +0100

    updated nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,16 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: ["~/assets/css/tailwind.css"],
-    build: {
-        postcss: {
-            postcssOptions: {
-                plugins: {
-                    tailwindcss: {},
-                    autoprefixer: {},
-                },
-            },
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
         },
     },
     ssr: false,
-    publicRuntimeConfig: {
-        JSON_URL: process.env.JSON_URL
-    }
+    runtimeConfig: {
+        public: {
+            JSON_URL: process.env.JSON_URL
+        }
+    },
 })
