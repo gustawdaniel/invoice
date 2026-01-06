4ae1498a8683bca8dec1bbfd8bc4a45826f481a6
Daniel Gustaw <gustaw.daniel@gmail.com>
2026-01-06 11:15:04 +0100

    feat: Implement invoice CRUD operations with authentication middleware and S3 integration.

---
+++ back/src/services/s3.ts
@@ -0,0 +1,36 @@
+import {
+  PutObjectCommand,
+  PutObjectCommandInput,
+  PutObjectCommandOutput,
+  S3Client,
+} from '@aws-sdk/client-s3';
+import { config } from '../config';
+
+const spacesEndpoint = new URL(`https://${config.SPACE_ENDPOINT}`).toString();
+
+const composeUrl = (path: string): string =>
+  `https://${config.SPACE_BUCKET}.${config.SPACE_ENDPOINT}/${path}`;
+
+const s3 = new S3Client({
+  forcePathStyle: false,
+  region: config.SPACE_REGION,
+  endpoint: spacesEndpoint,
+  credentials: {
+    accessKeyId: config.SPACE_ACCESS_KEY,
+    secretAccessKey: config.SPACE_SECRET_KEY,
+  },
+});
+
+const putObject = async (
+  input: Omit<PutObjectCommandInput, 'Bucket'>,
+): Promise<PutObjectCommandOutput> => {
+  const command = new PutObjectCommand({
+    ACL: 'public-read',
+    Bucket: config.SPACE_BUCKET,
+    ...input,
+  });
+
+  return s3.send(command);
+};
+
+export { s3, composeUrl, putObject };
